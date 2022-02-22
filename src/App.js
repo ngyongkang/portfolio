d07db09e90a36f00e99284dd8d12a86c16738564
Ng Yong Kang <nyk.com.sg@gmail.com>
2022-02-22 11:01:10 +0800

    Massive commit on the 22/2/2022 11.00 AM

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import Projects from './Projects';
+import Social_Media from "./Social_Media";
+import profile from "./assets/profile.png";
+import Title from "./Title";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component
+{
+    //Make sure to call super or else code will break.
+    state = {displayBio: false};
+    
+
+    /* 
+    constructor()
+    {
+        super();
+        this.state = { displayBio: false }; this line does the same thing as the line above.
+        excepts that it is locked within this constructor method. The above is a better practice
+        by initiaising a variable for the entire class to use. Basically, a global variable.
+
+        //console.log('Component this', this); line to show info on the console on the web gui.
+
+        //Line will bind an object to a method outside to allow use of the object methods and variables.
+        this.toggleBio = this.toggleBio.bind(this);
+    }
+    */
+    /*
+    readMore()
+    {
+        Bad practice to alter a state directly, do not do this. ***
+        NEVER DIRECTLY MODIFY A REACT STATE!!!!. ******
+        THE FOLLOWING LINE IS WRONGG
+        this.state.displayBio = true; <-----------
+
+        The reason why this is a bad practice is because the
+        setState method re-calls the render() method at the end of setState(),
+        This is important as it allows the application to render the new updated
+        values in the state while just updating the values directly will not 
+        render the new values in leaving the appearance of the website unchanged.
+        
+
+        this.setState({displayBio: true});
+        console.log('readMore this', this);
+    }
+    */
+
+    /*
+    * Note class methods don't have logic to create their own objects Therefore, 
+    * class methods written this way is essentially a convenient way to click various methods
+    * to fire like a script.
+    * Important: It is not an object or class by itself. **
+    * Can link methods to objects variables above by typing = () =>
+    */
+    toggleBio = () =>
+    {
+        /*
+        * This line of code allows the object state to change to the opposition option of it's
+        * currently stored option. For example, if displayBio state is false, the following
+        * code would change it to true and vice versa. 
+        */
+        this.setState({displayBio: !this.state.displayBio});
+    }
+
+    render()
+    {  
+        return(
+            <div>
+                <img src={profile} alt='profile' className="profile"/>
+                <h1>Hello!</h1>
+                <p>My name is Ng Yong Kang.</p>
+                <Title/>
+                <p>I hope to learn valuable skills from this ReactJS course. :)</p>
+                {
+                    this.state.displayBio ? (
+                    <div>
+                        <p>I live in Singapore and I just got hired as a programmer.</p>
+                        <p>My main language is Java and I am currently learning React JS.</p>
+                        <p>I don't really like coding much but these are some things I do like</p>
+                        <p>gaming, anime, kdrama, my family and of course girls</p>
+                        <button onClick={this.toggleBio}>Show less</button>
+                    </div>
+                    ) : (
+                        <div>
+                            <button onClick={this.toggleBio}>Show more</button>
+                        </div>
+                    )
+                }
+                <hr/>
+                <Projects/> 
+                <hr/>
+                <Social_Media/>
+            </div>
+        )
+    }
 }
 
+/*
+* The export default line allows other files in the codebase to access this component.
+* By delcaring this App component as the default export of the file. Other files iwll 
+* be able to import the app just like we imported react without the curly braces from
+* the react module.
+*/
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
+//index.js is not a default file the index is a naming convention for this project default file.
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//CSS can be imported directly to the javascript by importing the css file locally.
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App/>,document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+//Example class and inheritance example.
+/*
+class Animal
+{
+    constructor(name,age) 
+    {
+        this.name =name;
+        this.age =age;
+    }
+
+    speak()
+    {
+        console.log('I am', this.name, 'and i am', this.age, 'years old');   
+    }
+}
+
+const animal1 = new Animal('Puma',32);
+animal1.speak();
+
+console.log(animal1);
+
+class Lion extends Animal
+{
+    constructor(name,age,fur,speed)
+    {
+        super(name,age);
+        this.fur = fur;
+        this.speed = speed;
+    }
+
+    roar()
+    {
+        console.log("ROARRRRRRR I HAVE", this.fur, 'fur , and I can run', this.speed, 'miles an hour!');
+    }
+}
+
+const lion1 = new Lion('Simba',20,'brown',25);
+lion1.speak();
+lion1.roar();
+console.log(lion1);
+*/
